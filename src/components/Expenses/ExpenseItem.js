93e6da083be36b82f9c9c801c85866d5d0f03226
Alli Kellerman <kellermanalli@gmail.com>
2023-03-26 18:02:04 -0500

    Added button

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -13,6 +13,7 @@ function ExpenseItem(props) {
                 <h2>{props.title}</h2>
                 <div className="expense-item__price">${props.amount}</div>
             </div>
+            <button>Change title:</button>
         </Card>
     );
 }
